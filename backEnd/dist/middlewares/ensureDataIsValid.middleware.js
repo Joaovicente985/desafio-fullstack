9bdbbeb8d4e809c03193406e8699582075e60551
João Vicente Horst <109743483+Joaovicente985@users.noreply.github.com>
2023-05-28 22:17:38 -0300

    Delete backEnd/dist directory

--- backEnd/dist/middlewares/ensureDataIsValid.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateBodyMiddleware = void 0;
-const validateBodyMiddleware = (schema) => (req, res, next) => {
-    const validatedBody = schema.parse(req.body);
-    req.body = validatedBody;
-    return next();
-};
-exports.validateBodyMiddleware = validateBodyMiddleware;
--- backEnd/dist/routes/login.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginRoutes = void 0;
-const express_1 = require("express");
-const login_controller_1 = require("../controllers/login.controller");
-const loginRoutes = (0, express_1.Router)();
-exports.loginRoutes = loginRoutes;
-loginRoutes.post("", login_controller_1.loginUserController);
